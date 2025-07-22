f550392a1fff3492301ce06390ab4dbd0051a24a
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-07-22 21:49:41 +0800

    -reduced y value

--- data/aggregated_data_for_prophet.sql
+++ data/aggregated_data_for_prophet.sql
@@ -4,3 +4,13 @@ CREATE SCHEMA capstone_smart_vehicle_detection;
 USE capstone_smart_vehicle_detection;
 
 SHOW TABLES;
+
+-- drop kasi test plng
+-- DROP TABLE IF EXISTS vehicle_aggregated;
+
+SELECT COUNT(*) FROM vehicle_aggregated as v WHERE v.y = 30;
+
+-- check entry
+SELECT * FROM vehicle_aggregated;
+-- check datatypes
+SHOW CREATE TABLE vehicle_aggregated;
