d8a2d0cb5f1822d0ea2b348e4ab1cc2caac4cc76
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-08-07 17:52:05 +0800

    -registered a middleware. -registered exception

--- src/app/main.py
+++ src/app/main.py
@@ -7,17 +7,15 @@ from src.app.db.base import Base
 from src.app.models.user import User  # ensure it's imported so Base knows the model
 
 # global exceptions
-from src.app.exceptions.custom_exceptions import (
-  DuplicateEntryException, ResourceNotFoundException,
-  UnauthorizedAccessException, InternalServerError
-)
-from src.app.exceptions.error_handler import (
-  duplicate_entry_exception_handler, internal_server_error_handler,
-  resource_not_found_handler, unauthorized_access_handler
-)
+from src.app.exceptions.custom_exceptions import *
+from src.app.exceptions.error_handler import *
+
+# middlware
+from src.app.middleware.jwt_filter_middleware import JWTFilterMiddleware
 
 # routers
 from src.app.routes.user_router import user_router
+from src.app.routes.dashboard_api_router import dashboard_router
 
 @asynccontextmanager
 async def life_span(app: FastAPI):
@@ -35,11 +33,16 @@ async def life_span(app: FastAPI):
 
 app = FastAPI(lifespan=life_span)
 
+# middlwares
+app.add_middleware(JWTFilterMiddleware)
+
 # user router
 app.include_router(user_router)
+app.include_router(dashboard_router)
 
 # regiustering global exeception handler
 app.add_exception_handler(InternalServerError, internal_server_error_handler)
 app.add_exception_handler(ResourceNotFoundException, resource_not_found_handler)
 app.add_exception_handler(DuplicateEntryException, duplicate_entry_exception_handler)
 app.add_exception_handler(UnauthorizedAccessException, unauthorized_access_handler)
+app.add_exception_handler(ForbiddenAccessException, forbidden_access_handler)
