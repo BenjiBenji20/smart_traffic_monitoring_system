cdb1b72d03eb8cf91f304fbcef0bb1172383f926
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-08-07 18:16:41 +0800

    -include refresh token to response in routes

--- src/app/schemas/token_schema.py
+++ src/app/schemas/token_schema.py
@@ -6,9 +6,11 @@ from datetime import datetime
 
 
 class Token(BaseModel):
+  # return 2 different token
   access_token: str
+  refresh_token: str
   token_type: str
-
+  
   class Config:
     from_attributes = True
 
