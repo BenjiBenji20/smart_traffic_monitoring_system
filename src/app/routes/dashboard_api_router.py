1c977f99530abff5867bafa0b965474d4159d62e
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-08-08 17:34:54 +0800

    -created routes to fetch current user profile

--- src/app/routes/dashboard_api_router.py
+++ src/app/routes/dashboard_api_router.py
@@ -1,24 +1,22 @@
 from fastapi import APIRouter, Depends, Request
-from sqlalchemy.ext.asyncio import AsyncSession
-import jwt
 
+from src.app.models.user import User
+from src.app.schemas.user_schema import UserSchema
+from src.app.exceptions.custom_exceptions import *
 from src.app.db.db_session import get_async_db
 from src.app.services.auth_service import get_current_user, oauth2_scheme
-"""
-  simulate stateless access by accessing fake db and 
-  respond it by accessing private api using jwt as header bearer
-"""
 
-fake_db: dict = {
-  "username": "Benji_09",
-  "complete_name": "Benji Canones",
-  "password": "BenjiBenji"
-}
 
 dashboard_router = APIRouter(prefix="/api/dashboard")
 
 # enables stateless access
 # request object is a middleware from middleware.jwt_filter_middleware.py
-@dashboard_router.get("/user")
-async def get_current_user(request: Request): 
-  return request.state.user  # Comes from middleware
+@dashboard_router.get("/user-profile", response_model=UserSchema)
+async def get_user_profile(user: User = Depends(get_current_user)): 
+  if not user:
+    raise ResourceNotFoundException("No user found.")
+  return user
+
+
+# get traffic prediction
+# from src.traffic_ai.traffic_prediction_json_bldr.py
