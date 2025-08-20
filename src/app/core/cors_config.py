a25cd434a0afb83b15d943b9e1c25255f1ace3a8
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-08-20 09:51:13 +0800

    -allow general origin for flutter app

--- src/app/core/cors_config.py
+++ src/app/core/cors_config.py
@@ -5,7 +5,7 @@ from src.app.core.address import CLIENT_ORIGINS
 def cors_middleware(app: FastAPI):
   app.add_middleware(
     CORSMiddleware,
-    allow_origins=CLIENT_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], # GET, POST, PUT, DELETE, OPTIONS
     allow_headers=["Authorization", "Content-Type"] # Authorization, Content-Type, etc.
