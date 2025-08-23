5fd7547d7c1c16836b8109a869c64cd4538c0196
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-08-23 18:36:33 +0800

    -created get history record by id and update version name field api endpoints

--- src/app/routes/dashboard_history_router.py
+++ src/app/routes/dashboard_history_router.py
@@ -1,11 +1,15 @@
 import logging
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.app.db.db_session import get_async_db
-from src.app.schemas.history_schema import HistoryListSchema, HistoryResponseSchema
-from src.app.services.history_service import get_all_history_service
+from src.app.schemas.history_schema import *
+from src.app.services.history_service import (
+  get_all_history_service, 
+  get_history_by_id_service, 
+  update_version_name_service
+)
 
 
 dashboard_history_router = APIRouter(
@@ -23,4 +27,42 @@ async def get_all_history(db: AsyncSession = Depends(get_async_db)):
     )
   except Exception as e:
     logging.error(f"Error in get_all_history endpoint: {e}")
+    raise
+  
+
+@dashboard_history_router.get("/one-history", response_model=HistorySchema)
+async def get_history_by_id(id: str, db: AsyncSession = Depends(get_async_db)):
+  try:
+    response = await get_history_by_id_service(id, db)
+    
+    return HistorySchema(
+      version_name=response.version_name,
+      prediction_summary=response.prediction_summary,
+      prediction_detail=response.prediction_detail,
+      ai_recommendation=response.ai_recommendation
+    )
+  except Exception as e:
+    logging.error(f"Error in get_history_by_id endpoint: {e}")
+    raise
+    
+    
+@dashboard_history_router.put("/update-version-name", response_model=HistorySchema)
+async def update_version_name(id: str, new_ver_name: str, db: AsyncSession = Depends(get_async_db)):
+  try:
+    is_updated = await update_version_name_service(id, new_ver_name, db)
+    
+    if not is_updated:
+      raise HTTPException(status_code=400, detail="Failed to update version name")
+    
+    # query the updated version name to use by client
+    response = await get_history_by_id_service(id, db)
+    
+    return HistorySchema(
+      version_name=response.version_name,
+      prediction_summary=response.prediction_summary,
+      prediction_detail=response.prediction_detail,
+      ai_recommendation=response.ai_recommendation
+    )
+  except Exception as e:
+    logging.error(f"Error in update_version_name endpoint: {e}")
     raise
--- src/app/services/history_service.py
+++ src/app/services/history_service.py
@@ -1,11 +1,11 @@
 from typing import List
 import logging
-from sqlalchemy import UUID, select
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.app.exceptions.custom_exceptions import InternalServerError
+from src.app.exceptions.custom_exceptions import ResourceNotFoundException
 from src.app.models.history import History
-from src.app.schemas.history_schema import HistoryResponseSchema
+from src.app.schemas.history_schema import HistoryResponseSchema, HistorySchema
 
 async def get_all_history_service(db: AsyncSession) -> List[HistoryResponseSchema]:
   try:
@@ -32,5 +32,52 @@ async def get_all_history_service(db: AsyncSession) -> List[HistoryResponseSchem
   except Exception as e:
     logging.error(f"An error occured while fetching all history record: {e}")
     return []
+  
+  
+async def get_history_by_id_service(id: str, db: AsyncSession) -> HistorySchema:
+  try:
+    result = await db.execute(
+      select(History)
+      .where(History.id == id)
+    )
     
+    response = result.scalar_one_or_none()
     
+    if not response:
+      raise ResourceNotFoundException(f"History record not found by id: {id}")
+    
+    return response
+  
+  except Exception as e:
+    logging.error(f"An error occured while fetching history record by id:{id} Error: {e}")
+    raise
+    
+
+async def update_version_name_service(id: str, new_ver_name: str, db: AsyncSession) -> bool:
+  try:
+    result = await db.execute(select(History).where(History.id == id))
+    response = result.scalar_one_or_none()
+    
+    if not response:
+      raise ResourceNotFoundException(f"History record not found by id: {id}")
+    
+    # validate if still same name
+    if response.version_name == new_ver_name:
+      return True # immediately stop this bullshit
+    
+    # update version_name field only
+    stmt = (
+      update(History)
+      .where(History.id == id)
+      .values(version_name=new_ver_name)
+      .execution_options(synchronize_session="fetch")
+    )
+    
+    result = await db.execute(stmt)
+    await db.commit()
+    
+    return result.rowcount > 0  # True if updated, False if not
+  except Exception as e:
+    await db.rollback() # rollback if something went wrong
+    logging.error(f"Error updating version name: {e}")
+    raise
