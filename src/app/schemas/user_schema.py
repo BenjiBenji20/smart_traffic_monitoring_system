a410bcf1b4e4c79f1bacd07c51add2ec3fbdc2fa
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-11-01 20:29:21 +0800

    created update profile schema that accepts optional field

--- src/app/schemas/user_schema.py
+++ src/app/schemas/user_schema.py
@@ -1,3 +1,5 @@
+import re
+from typing import Optional
 from pydantic import BaseModel, Field, field_validator
 from datetime import datetime
 import regex
@@ -39,12 +41,57 @@ class RegisterUserSchema(AuthUserSchema):
     
     return val
   
+
+class UpdateInfoSchema(BaseModel):
+  """Use for updating that requires field optional"""
+  username: Optional[str] = Field(
+    default=None,
+    min_length=3,
+    max_length=20,
+    pattern=r"^[a-zA-Z0-9_]{3,20}$"
+  )
+  
+  password: Optional[str] = Field(
+    default=None,
+    min_length=8,
+    max_length=50
+  )
+  role: Optional[Role] = None
+  complete_name: Optional[str] = Field(default=None, max_length=50)
+  complete_address: Optional[str] = Field(default=None, max_length=100)
+  age: Optional[int] = Field(default=None, gt=0, le=120)
+
+  @field_validator("*", mode='before')
+  @classmethod
+  def skip_validation_for_none(cls, value, info):
+    """Skip validation for None values"""
+    if value is None:
+      return value
+    return value
+
+  # Validators will only run for non-None values due to the above validator
+  @field_validator("password")
+  @classmethod
+  def validate_password(cls, value):
+    pattern = r"^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@#$%^&+=]{8,}$"
+    if not re.match(pattern, value):
+      raise ValueError("Password must be at least 8 characters long, include one uppercase letter and one number.")
+    return value
+
+  @field_validator("complete_name", "complete_address")
+  @classmethod
+  def validate_name_and_address(cls, val):
+    pattern = r"^[a-zA-Z][a-zA-Z'\-,. ]*$"
+    if not re.match(pattern, val):
+      raise ValueError(f"Must be valid: {val}")
+    return val
+  
   
 # request dto
 class UpdateUserProfileSchema(BaseModel):
   username: str = Field(..., min_length=3, max_length=50)
   password: str = Field(..., min_length=8)
-  update_info: RegisterUserSchema  # new info of user being updated
+  update_info: UpdateInfoSchema  # new info of user being updated
 
 
 # for response dto
